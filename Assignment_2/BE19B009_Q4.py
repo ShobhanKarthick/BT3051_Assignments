f7e7a7aae50dac66a8f551330c3d56751f1d8f1d
Shobhan Karthick <loganshobhanwolverine@gmail.com>
2021-09-17 00:52:51 +0530

    Finished Queestion 4

--- Assignment_2/BE19B009_Q4.py
+++ Assignment_2/BE19B009_Q4.py
@@ -1,38 +1,110 @@
 operators = ["(", "^", "/", "*", "+", "-", ")"]
-postfixStack = []
-operatorStack = []
 
 def InfixToPostfix(infix):
     i = 0
-    while (i <= len(infix)):
+    postfixStack = []
+    operatorStack = []
+    while i in range(len(infix)+1):
         number = ""
-        j = i 
-        while(j < len(infix)):
-            if (infix[j] not in operators):
-                number += infix[j]
-                j += 1
-            else:
-                break
-
-        if (number != ""):
-            if(len(operatorStack) == 0):
-                postfixStack.append(number)
-            
-            print(number)
-        i = j+1
         try:
-            if(len(operatorStack) == 0):
-                postfixStack.append(infix[i-1])
-            else:
-                operatorStack.append(infix[i-1])
-            print(infix[i-1])
+            while not infix[i] in operators:
+                if(i < len(infix)):
+                    number += infix[i]
+                    i += 1
+                else:
+                    break
+
+            if(number != ""):
+                # print("A:", number)
+                postfixStack.append(int(number))
+                # else:
+
+            if(infix[i]):
+                # print(infix[i])
+                if(len(postfixStack) == 0):
+                    postfixStack.append(infix[i])
+                elif(len(operatorStack) == 0):
+                    operatorStack.append(infix[i])
+                elif(infix[i] == "(" or infix[i] == "^"):
+                    operatorStack.append(infix[i])
+                elif(infix[i] == "*" or infix[i] == "/"):
+                    if(len(operatorStack) != 0):
+                        try:
+                            while((operatorStack[-1] == "^" or operatorStack[-1] == "*" or operatorStack[-1] == "/")):
+                                postfixStack.append(operatorStack.pop())
+                            operatorStack.append(infix[i])
+                        except:
+                            operatorStack.append(infix[i])
+
+                elif(infix[i] == "-" or infix[i] == "+"):
+                    if(len(operatorStack) != 0):
+                        try:
+                            while((operatorStack[-1] == "+" or operatorStack[-1] == "-" or operatorStack[-1] == "/" or operatorStack[-1] == "*" or operatorStack[-1] == "^")):
+                                postfixStack.append(operatorStack.pop())
+                        except:
+                            # print("hello")
+                            operatorStack.append(infix[i])
+                        
+
+                elif(infix[i] == ")"):
+                    if(len(operatorStack) != 0):
+                        try:
+                            while(len(operatorStack) != 0 and operatorStack[-1] != "("):
+                                postfixStack.append(operatorStack.pop())
+                            operatorStack.pop()
+                        except:
+                            operatorStack.pop()
+
+                else:
+                    postfixStack.append(infix[i])
         except:
-            break
+            if(number != ""):
+                # print("B:", number)
+                postfixStack.append(int(number))
+                if(len(operatorStack) != 0 ):
+                        postfixStack.append(operatorStack.pop())
 
-InfixToPostfix("32^43/(55*66+72)+10")
-print(operatorStack)
-print(postfixStack)
+        i += 1
+    return postfixStack
+
+def EvaluatePostfix(postfix):
+
+    evalStack = []
+    for i in range(len(postfix)):
+        if not postfix[i] in operators:
+            evalStack.append(postfix[i])
+
+        elif(postfix[i] == '+'):
+            x = evalStack.pop()
+            y = evalStack.pop()
+            evalStack.append(y + x)
+        
+        elif(postfix[i] == '-'):
+            x = evalStack.pop()
+            y = evalStack.pop()
+            evalStack.append(y - x)
+        
+        elif(postfix[i] == '*'):
+            x = evalStack.pop()
+            y = evalStack.pop()
+            evalStack.append(y * x)
+        
+        elif(postfix[i] == '/'):
+            x = evalStack.pop()
+            y = evalStack.pop()
+            evalStack.append(y / x)
+        
+        elif(postfix[i] == '^'):
+            x = evalStack.pop()
+            y = evalStack.pop()
+            evalStack.append(y ** x)
+
+    return evalStack.pop()
 
 
+# postfix = InfixToPostfix("42+43/(55-66*72^32)+100-3")
+postfix = InfixToPostfix("3^4/(5*6)+10")
+print("Postfix expression: ", postfix)
+answer = EvaluatePostfix(postfix)
+print("Value of expression: ", answer)
 
-    
